02fb397fe142e7fbcb0c8b45e69db23c0ff5a679
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-05-30 04:27:04 +0200

    [UPD] added emails's list in the page

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ createApp({
   data(){
     return{
       message:'aaaa',
+      emails:[],
     }
   },
-  methods:({
 
-  })
+  created(){
+    for(let i=0;i<10;i++){
+      axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=> {
+          this.emails.push(response.data.response);
+        })
+    }
+    
+  }
 }).mount('#app');
+
